1fb0e7b73f0a982b55f70e70ee4e6b6fb7017e8f
Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-11-01 19:47:41 +0200

    handle registration 8.3.6

--- public/js/register.js
+++ public/js/register.js
@@ -8,4 +8,25 @@
  *       - Use postOrPutJSON() function from utils.js to send your data back to server
  */
 
-const form = document.getElementById("register-form");
+document.getElementById("btnRegister").addEventListener("click", () => {
+    const form = document.getElementById("register-form");
+    const name = form.getElementById("name").value;
+    const email = form.getElementById("email").value;
+    const pass = form.getElementById("password").value;
+    const passConfirm = form.getElementById("passwordConfirmation").value;
+
+    if (pass !== passConfirm) {
+        createNotification("Password and password confirmation not match", 
+                            document.getElementById("notifications-container"), false);
+        
+    }
+    else {
+        form.reset();
+        const user = {
+            "name": name,
+            "email": email,
+            "password": pass
+        };
+        await postOrPutJSON("http://localhost:3000/api/users", "POST", user);
+    }
+});
