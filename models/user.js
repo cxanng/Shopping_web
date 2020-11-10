77beacd71260dc760e30bd4a2c3264144cb973d4
Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-11-11 01:53:57 +0200

    add implementation for model user.js, 9.4

--- models/user.js
+++ models/user.js
@@ -4,6 +4,47 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   // TODO: 9.4 Implement this
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is required",
+    validate: [
+      function(input) {
+        return input.length <= 50;
+      }
+    ]
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: "Email is required",
+    unique: "Email has been used",
+    match: [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/]
+  },
+  password: {
+    type: String,
+    required: "Password is required",
+    validate: [
+      function(input) {
+        return input.length >= 10;
+      },
+      "Password should be longer."
+    ],
+    set: [(input) => {
+      if (input.length >= 10) {
+        const salt = bcrypt.genSaltSync(10);
+        return bcrypt.hashSync(input, salt);
+      }
+    }]
+  },
+  role: {
+    type: String,
+    required: "Role is required",
+    default: "customer",
+    enum: ["admin", "customer"],
+    trim: true,
+    lowercase: true
+  }
 });
 
 /**
@@ -14,6 +55,7 @@ const userSchema = new Schema({
  */
 userSchema.methods.checkPassword = async function (password) {
   // TODO: 9.4 Implement this
+  return bcrypt.compare(password, this.password);
 };
 
 // Omit the version key when serialized to JSON
