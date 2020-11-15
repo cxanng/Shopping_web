7e17f2a6315f99dd3c4a08cf1dd394ee8a51abda
Anh Nguyen <anh.t.nguyen@tuni.fi>
2020-11-15 18:52:43 +0200

    Adding implementation for getAllProduct, 10.1

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,5 @@
+const responseUtils = require("../utils/responseUtils");
+
 /**
  * Send all products as JSON
  *
@@ -5,7 +7,9 @@
  */
 const getAllProducts = async response => {
   // TODO: 10.1 Implement this
-  throw new Error('Not Implemented');
+  // throw new Error('Not Implemented');
+  const products = require("../products.json").map(product => ({ ...product }));
+  return responseUtils.sendJson(response, products);
 };
 
 module.exports = { getAllProducts };
