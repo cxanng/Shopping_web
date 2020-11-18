43884f0b8a8b49415750ce7808bd2ba06d9e7b41
Duy Anh Vu <duy.a.vu@tuni.fi>
2020-11-18 22:49:46 +0200

    Add test cases for 10.3

---
+++ test/own/ui.test.js
@@ -0,0 +1,37 @@
+describe("Set product as favourite", () => {
+  it("The star icon should be yellow when user click first time", () => {
+    throw "Not implemented";
+  })
+
+  it("The favourite item should appear in favourite tab", () => {
+    throw "Not implemented";
+  })
+});
+
+describe("Search toolbar", () => {
+  it("Should show products with filtered name", () => {
+    throw "Not implemented";
+  });
+
+  it("Should show no products when the filter is invalid", () => {
+    throw "Not implemented";
+  });
+
+  it("Show all products when the bar is empty", () => {
+    throw "Not implemented";
+  });
+});
+
+describe("Report creation", () => {
+  it("The report form should contain necessary fields", () => {
+    throw "Not implemented";
+  });
+
+  it("The report should be visible to admin accounts, but not customers", () => {
+    throw "Not implemented";
+  });
+
+  it("Should show notification when report is created", () => {
+    throw "Not implemented";
+  });
+});
---
+++ test/own/utils.test.js
@@ -0,0 +1,9 @@
+describe("Sort functionalities", () => {
+  it("Should sort by name correspondingly", () => {
+    throw "Not implemented";
+  });
+
+  it("Should sort by price accordingly", () => {
+    throw "Not implemented";
+  });
+});
