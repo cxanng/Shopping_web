c5e65741b165d2b284d8a86dea961c980a9f0b49
Duy Anh Vu <duy.a.vu@tuni.fi>
2020-12-05 01:54:39 +0200

    Fix eslint errors in products.js

--- public/js/products.js
+++ public/js/products.js
@@ -4,152 +4,195 @@ const container = document.getElementById("products-container");
 const URL = "http://localhost:3000/api/products";
 
 const renderProduct = products => {
-    products.forEach(product => {
-        const id = product._id;
-        const newProduct = template.content.cloneNode(true);
+  products.forEach(product => {
+    const id = product._id;
+    const newProduct = template.content.cloneNode(true);
 
-        const itemRow = newProduct.querySelector("div");
-        itemRow.setAttribute("id", `product-${id}`);
+    const itemRow = newProduct.querySelector("div");
+    itemRow.setAttribute("id", `product-${id}`);
 
-        const name = itemRow.querySelector("h3");
-        name.setAttribute("id", `name-${id}`);
-        name.innerText = product.name;
+    const name = itemRow.querySelector("h3");
+    name.setAttribute("id", `name-${id}`);
+    name.innerText = product.name;
 
-        const image = itemRow.querySelector(".product-image");
-        image.setAttribute("id", `image-${id}`);
-        image.setAttribute("src", product.image);
+    const image = itemRow.querySelector(".product-image");
+    image.setAttribute("id", `image-${id}`);
+    image.setAttribute("src", product.image);
 
-        const description = itemRow.querySelector(".product-description");
-        description.setAttribute("id", `description-${id}`);
-        description.innerText = product.description;
+    const description = itemRow.querySelector(".product-description");
+    description.setAttribute("id", `description-${id}`);
+    description.innerText = product.description;
 
-        const price = itemRow.querySelector(".product-price");
-        price.setAttribute("id", `price-${id}`);
-        price.innerText = product.price;
+    const price = itemRow.querySelector(".product-price");
+    price.setAttribute("id", `price-${id}`);
+    price.innerText = product.price;
 
-        const addToCart = itemRow.querySelector(".add-to-cart-button");
-        addToCart.setAttribute("id", `add-to-cart-${id}`);
-        addToCart.innerText = "Add to cart";
-        addToCart.addEventListener("click", () => addProduct(product));
+    const addToCart = itemRow.querySelector(".add-to-cart-button");
+    addToCart.setAttribute("id", `add-to-cart-${id}`);
+    addToCart.innerText = "Add to cart";
+    addToCart.addEventListener("click", () => addProduct(product));
 
-        const modifyItem = itemRow.querySelector(".modify-product-button");
-        modifyItem.setAttribute("id", `modify-${id}`);
-        modifyItem.addEventListener("click", () => modifyHandle(id));
+    const modifyItem = itemRow.querySelector(".modify-product-button");
+    modifyItem.setAttribute("id", `modify-${id}`);
+    modifyItem.addEventListener("click", () => modifyHandle(id));
 
-        const deleteItem = itemRow.querySelector(".delete-product-button");
-        deleteItem.setAttribute("id", `delete-${id}`);
-        deleteItem.addEventListener("click", () =>deleteHandle(id));
+    const deleteItem = itemRow.querySelector(".delete-product-button");
+    deleteItem.setAttribute("id", `delete-${id}`);
+    deleteItem.addEventListener("click", () => deleteHandle(id));
 
-        container.appendChild(itemRow);
-    });
-}
+    container.appendChild(itemRow);
+  });
+};
 
 const addProduct = ({ _id, name }) => {
-    addProductToCart(_id);
-    createNotification(`Added ${name} to cart!`, "notifications-container");
-}
+  addProductToCart(_id);
+  createNotification(`Added ${name} to cart!`, "notifications-container");
+};
 
 const productLoad = async () => {
-    const products = await getJSON(URL);
-    renderProduct(products);
-}
+  const products = await getJSON(URL);
+  renderProduct(products);
+};
 
-const deleteHandle = async (id) => {
-    const url = `api/products/${id}`;
-    deleteResourse(url).then(response => {
-        if(response) {
-            createNotification("Deleted product "+ response.name, "notifications-container", true);
-            container.querySelectorAll(`#product-${id}`).forEach(element => element.remove());
-            document.getElementById("modify-product").querySelectorAll("edit-product-form").forEach(element => element.remove());
+const deleteHandle = async id => {
+  const url = `api/products/${id}`;
+  const response = await deleteResourse(url);
+  if (response) {
+    createNotification(
+      `Deleted product ${response.name}`,
+      "notifications-container",
+      true
+    );
+    container
+      .querySelectorAll(`#product-${id}`)
+      .forEach(element => element.remove());
+    document
+      .getElementById("modify-product")
+      .querySelectorAll("edit-product-form")
+      .forEach(element => element.remove());
+  }
+};
+
+const modifyHandle = async id => {
+  const url = `api/products/${id}`;
+  const productInfo = await getJSON(url);
+  const formTemplate = document.getElementById("form-template");
+  const modifyContainer = document.getElementById("modify-product");
+
+  const cloneDiv = formTemplate.content.cloneNode(true);
+
+  const heading = cloneDiv.querySelector("h2");
+  heading.innerHTML = `Modify product ${productInfo.name}`;
+
+  const productId = cloneDiv.querySelector("#id-input");
+  productId.value = productInfo._id;
+
+  const name = cloneDiv.querySelector("#name-input");
+  name.value = productInfo.name;
+
+  const description = cloneDiv.querySelector("#description-input");
+  description.value = productInfo.description;
+
+  const image = cloneDiv.querySelector("#image-input");
+  image.value = productInfo.image;
+
+  const price = cloneDiv.querySelector("#price-input");
+  price.value = productInfo.price;
+
+  modifyContainer.innerHTML = "";
+  modifyContainer.appendChild(cloneDiv);
+
+  document.getElementById("update-button").addEventListener("click", e => {
+    e.preventDefault();
+    if (!image.value.match(/^(http|https):\/\/\S+\.\w{2,}\/*\S*$/)) {
+      createNotification(
+        "Provide correct Url",
+        "notifications-container",
+        false
+      );
+    } else {
+      const data = {
+        ...productInfo,
+        name: name.value,
+        description: description.value,
+        image: image.value,
+        price: Number(price.value)
+      };
+      postOrPutJSON(url, "PUT", data).then(response => {
+        if (response) {
+          createNotification(
+            `Updated product ${response.name}`,
+            "notifications-container",
+            true
+          );
+          document.getElementById(`name-${response._id}`).textContent =
+            response.name;
+          document.getElementById(`description-${response._id}`).textContent =
+            response.description;
+          document.getElementById(`price-${response._id}`).textContent =
+            response.price;
+          document
+            .getElementById(`image-${response._id}`)
+            .setAttribute("src", response.image);
+          document
+            .getElementById("modify-product")
+            .querySelectorAll("edit-product-form")
+            .forEach(element => element.remove());
         }
-    })
-}
+      });
+    }
+  });
+};
 
-const modifyHandle = async (id) => {
-    const url = "api/products/" + id;
-    const productInfo = await getJSON(url);
-    const template = document.getElementById("form-template");
-    const modifyContainer = document.getElementById("modify-product");
+document
+  .getElementById("sort-by-name-button-increase")
+  .addEventListener("click", () => {
+    Array.from(tbody.querySelectorAll("tr:nth-child(n+1)"))
+      .sort((a, b) => {
+        const text1 = a.querySelectorAll("td")[col].innerText;
+        const text2 = b.querySelectorAll("td")[col].innerText;
+        return text1.localeCompare(text2);
+      })
+      .forEach(tr => tbody.appendChild(tr));
+    // const nameSortedProduct = products.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    // container.innerHTML = "";
+    // renderProduct(nameSortedProduct);
+  });
 
-    const cloneDiv = template.content.cloneNode(true);
-
-    const heading = cloneDiv.querySelector("h2")
-    heading.innerHTML = `Modify product ${productInfo.name}`;
-
-    const productId = cloneDiv.querySelector("#id-input");
-    productId.value = productInfo._id;
-
-    const name = cloneDiv.querySelector("#name-input");
-    name.value = productInfo.name;
-
-    const description = cloneDiv.querySelector("#description-input");
-    description.value = productInfo.description;
-
-    const image = cloneDiv.querySelector("#image-input");
-    image.value = productInfo.image;
-
-    const price = cloneDiv.querySelector("#price-input");
-    price.value = productInfo.price;
-
-    modifyContainer.innerHTML = "";
-    modifyContainer.appendChild(cloneDiv);
-
-    document.getElementById("update-button").addEventListener('click', (e)=> {
-        e.preventDefault();
-        if (!image.value.match(/^(http|https):\/\/\S+\.\w{2,}\/*\S*$/)){
-            createNotification("Provide correct Url", "notifications-container", false);
-        }
-        else{            
-        const data = { ...productInfo,
-            name:name.value,
-            description:description.value,
-            image:image.value,
-            price:Number(price.value) 
-        };
-        postOrPutJSON(url,"PUT",data).then(response => {
-            if(response){
-                createNotification(`Updated product ${response.name}`, "notifications-container", true);
-                document.getElementById(`name-${response._id}`).textContent= response.name;
-                document.getElementById(`description-${response._id}`).textContent= response.description;
-                document.getElementById(`price-${response._id}`).textContent= response.price;
-                document.getElementById(`image-${response._id}`).setAttribute("src",response.image);
-                document.getElementById("modify-product").querySelectorAll("edit-product-form").forEach(element => element.remove());
-            }
-        });
-        }
-    })
-}
-
-document.getElementById("sort-by-name-button-increase").addEventListener("click", async (e) => {
+document
+  .getElementById("sort-by-name-button-decrease")
+  .addEventListener("click", async e => {
     e.preventDefault();
     const products = await getJSON(URL);
-    const nameSortedProduct = products.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    const nameSortedProduct = products.sort((a, b) =>
+      a.name < b.name ? 1 : -1
+    );
     container.innerHTML = "";
     renderProduct(nameSortedProduct);
-})
+  });
 
-document.getElementById("sort-by-name-button-decrease").addEventListener("click", async (e) => {
+document
+  .getElementById("sort-by-price-button-increase")
+  .addEventListener("click", async e => {
     e.preventDefault();
     const products = await getJSON(URL);
-    const nameSortedProduct = products.sort((a, b) => (a.name < b.name) ? 1 : -1);
+    const nameSortedProduct = products.sort((a, b) =>
+      a.price > b.price ? 1 : -1
+    );
     container.innerHTML = "";
     renderProduct(nameSortedProduct);
-})
+  });
 
-document.getElementById("sort-by-price-button-increase").addEventListener("click", async (e) => {
+document
+  .getElementById("sort-by-price-button-decrease")
+  .addEventListener("click", async e => {
     e.preventDefault();
     const products = await getJSON(URL);
-    const nameSortedProduct = products.sort((a, b) => (a.price > b.price) ? 1 : -1);
+    const nameSortedProduct = products.sort((a, b) =>
+      a.price < b.price ? 1 : -1
+    );
     container.innerHTML = "";
     renderProduct(nameSortedProduct);
-})
-
-document.getElementById("sort-by-price-button-decrease").addEventListener("click", async (e) => {
-    e.preventDefault();
-    const products = await getJSON(URL);
-    const nameSortedProduct = products.sort((a, b) => (a.price < b.price) ? 1 : -1);
-    container.innerHTML = "";
-    renderProduct(nameSortedProduct);
-})
+  });
 
 productLoad();
